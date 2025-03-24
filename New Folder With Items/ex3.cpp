5ca62df8c966a51272ea9b89cd6b30e1adf85730
BaoNTfit <thanhbao0905@gmail.comgit commit --amend --reset authora>
2025-03-24 22:01:37 +0700

    them file

---
+++ New Folder With Items/ex3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <string>
+using namespace std;
+void generateBinaryStrings(int n, string str);
+int main() {
+    int n;
+    cin >> n;
+    generateBinaryStrings(n, ""); // Start with an empty string
+    return 0;
+}
+
+
+void generateBinaryStrings(int n, string str) {
+    if (n == 0) {
+        cout << str << endl;
+    } else {
+        generateBinaryStrings(n - 1, str + '0');
+        generateBinaryStrings(n - 1, str + '1');
+    }
+}
