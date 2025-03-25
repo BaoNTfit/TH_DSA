85eed210ba5260fa5b1a190e01b0d145daa25618
BaoNTfit <thanhbao0905@gmail.comgit commit --amend --reset authora>
2025-03-25 21:39:23 +0700

    2 Bai con lai

---
+++ 24120023_Week01/ex5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+// Function to check if an array is sorted using recursion
+bool isSorted(int arr[], int n) {
+    // Base case: If there is only one or zero elements, it is sorted
+    if (n == 1 || n == 0) return true;
+// If first element is greater than the second, it's not sorted
+    if (arr[0] > arr[1])  return false;
+// Recursive call to check the rest of the array
+    return isSorted(arr + 1, n - 1);
+}
+int main() {
+    int n;
+    cin >> n;
+    int* a = new int [n];
+    for (int i = 0; i < n; i ++) cin >> a[i];
+    if (isSorted(a, n))
+    cout << "The array is sorted.\n";
+    else cout << "The array is NOT sorted.\n";
+    delete [] a;
+    return 0;
+}
